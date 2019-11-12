fda8b63870cc9607407a944fdf990865c1cd86c4
BalooSLU <42147187+BalooSLU@users.noreply.github.com>
2019-11-12 09:10:46 +0100

    included headers

--- main.c
+++ main.c
@@ -14,8 +14,17 @@
 //test
 
 #include<stdio.h>
-#include"tm4c1294ncpdt.h"
-#include"startup_disp.h"
+#include "startup_disp.h"
+#include "inc/tm4c1294ncpdt.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/systick.h"
+#include "driverlib/sysctl.h"
 
 #define DISP_WIDTH 480
 #define DISP_HIGHT 272
